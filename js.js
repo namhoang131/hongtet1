c170dcdc21808e0fa9120d55dfe8d46f7486f6c6
NamTool131 <143366877+namhoang131@users.noreply.github.com>
2023-08-31 08:21:30 +0700

    js.js

--- js.js
+++ js.js
@@ -1,29 +1,78 @@
-    $(document).ready(function() {
+$(document).ready(function() {
     var t = [{
-        name: "Mậu Tuất",
-        m1: Date.parse("Feb 16 2018 00:00:00")
-    }, {
-        name: "Kỷ Hợi",
-        m1: Date.parse("Feb 5 2019 00:00:00")
-    }, {
-        name: "Canh Tý",
-        m1: Date.parse("Jan 25 2020 00:00:00")
-    }, {
-        name: "Tân Sửu",
-        m1: Date.parse("Feb 12 2021 00:00:00")
-    }, {
-        name: "Nhâm Dần",
-        m1: Date.parse("Feb 1 2022 00:00:00")
-    }, {
-        name: "Quý Mão",
-        m1: Date.parse("Jan 22 2023 00:00:00")
-    }, {
+        
         name: "Giáp Thìn",
         m1: Date.parse("Feb 10 2024 00:00:00")
     }, {
         name: "Ất Tỵ",
         m1: Date.parse("Jan 29 2025 00:00:00")
-    }]
+    }, {
+        name: "Bính Ngọ",
+        m1: Date.parse("Feb 17 2026 00:00:00")
+    }, {
+        name: "Đinh Mùi",
+        m1: Date.parse("Feb 7 2027 00:00:00")
+    }, {
+        name: "Mậu Thân",
+        m1: Date.parse("Jan 26 2028 00:00:00")
+    }, {
+        name: "Kỷ Dậu",
+        m1: Date.parse("Feb 13 2029 00:00:00")
+    }, {
+        name: "Canh Tuất",
+        m1: Date.parse("Feb 2 2030 00:00:00")
+    }, {
+        name: "Tân Hợi",
+        m1: Date.parse("Jan 23 2031 00:00:00")
+    }, {
+        name: "Nhâm Tý",
+        m1: Date.parse("Feb 11 2032 00:00:00")
+    }, {
+        name: "Quý Sửu",
+        m1: Date.parse("Jan 31 2033 00:00:00")
+    }, {
+        name: "Giáp Dần",
+        m1: Date.parse("Feb 19 2034 00:00:00")
+    }, {
+        name: "Ất Mão",
+        m1: Date.parse("Feb 8 2035 00:00:00")
+    }, {
+        name: "Bính Thìn",
+        m1: Date.parse("Jan 28 2036 00:00:00")
+    }, {
+        name: "Đinh Tỵ",
+        m1: Date.parse("Feb 15 2037 00:00:00")
+    }, {
+        name: "Mậu Ngọ",
+        m1: Date.parse("Feb 4 2038 00:00:00")
+    }, {
+        name: "Kỷ Mùi",
+        m1: Date.parse("Jan 24 2039 00:00:00")
+    }, {
+        name: "Canh Thân",
+        m1: Date.parse("Feb 12 2040 00:00:00")
+    }, {
+        name: "Tân Dậu",
+        m1: Date.parse("Feb 1 2041 00:00:00")
+    }, {
+        name: "Nhâm Tuất",
+        m1: Date.parse("Feb 10 2042 00:00:00")
+    }, {
+        name: "Quý Hợi",
+        m1: Date.parse("Jan 30 2043 00:00:00")
+    }, {
+        name: "Giáp Tý",
+        m1: Date.parse("Feb 17 2044 00:00:00")
+    }, {
+        name: "Ất Sửu",
+        m1: Date.parse("Feb 6 2045 00:00:00")
+    }, {
+        name: "Bính Dần",
+        m1: Date.parse("Jan 26 2046 00:00:00")
+    }, {
+        name: "Đinh Mão",
+        m1: Date.parse("Jan 23 2047 00:00:00")
+    },]
       , e = t.length;
     setInterval(function() {
         for (var n = (new Date).getTime(), a = 0; a < e; a++) {
